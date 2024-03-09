4ec75baa98f336e76cb9027e9c634d462428ceb3
ruri <devruri@gmail.com>
2024-03-10 00:49:36 +0900

    tailwindsを使ったページを作成してみる

--- components/CategoryList.vue
+++ components/CategoryList.vue
@@ -1,9 +1,50 @@
+<script lang="ts">
+export default {
+  data: function() {
+    return {
+      categories: []
+    }
+  },
+  mounted() {
+    this.categories = [
+      {
+        id: 1,
+        name: 'カテゴリ1',
+        order_no: 1,
+      },
+      {
+        id: 2,
+        name: 'カテゴリ2',
+        order_no: 2,
+      }
+    ]
+  },
+};
+</script>
 <template>
-  <div>
-    <h3>カテゴリ一覧</h3>
-    <ul>
-      <li><NuxtLink to="/categories/1">カテゴリ1</NuxtLink></li>
-      <li><NuxtLink to="/categories/2">カテゴリ2</NuxtLink></li>
-    </ul>
+  <div class="border-r p-4">
+    <table>
+      <caption>
+        <div class="flex justify-between">
+          <div class="text-2xl">カテゴリ一覧</div>
+          <div>
+            <NuxtLink to="/categories/new">
+              <button class="middle none center rounded-lg bg-orange-500 py-1 px-3 font-sans text-xs font-bold text-white">新規追加</button>
+            </NuxtLink>
+          </div>
+        </div>
+      </caption>
+      <thead class="bg-white border-b">
+        <tr>
+          <th class="border border-slate-300 dark:border-slate-600 font-semibold px-4 py-1 text-slate-900 dark:text-slate-200 text-left">ID</th>
+          <th class="border border-slate-300 dark:border-slate-600 font-semibold px-4 py-1 text-slate-900 dark:text-slate-200 text-left">カテゴリ名</th>
+          <th class="border border-slate-300 dark:border-slate-600 font-semibold px-4 py-1 text-slate-900 dark:text-slate-200 text-left">ソート順位</th>
+          <td class="border border-slate-300 dark:border-slate-700 px-4 py-1 text-slate-500 dark:text-slate-400 text-cyan-600" colspan="2"></td>
+        </tr>
+      </thead>
+      <tbody>
+        <CategoryListNode v-for="category in categories" :category="category" :key="category.id" />
+      </tbody>
+    </table>
   </div>
 </template>
