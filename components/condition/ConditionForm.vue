cb4775938cec38bf4d9c8d3824e35e8afda7451a
ruri <devruri@gmail.com>
2024-03-12 17:35:06 +0900

    お譲り条件管理画面作成

---
+++ components/condition/ConditionForm.vue
@@ -0,0 +1,67 @@
+<template>
+  <div class="p-4">
+    <div class="antialiased text-gray-900">
+      <div class="max-w-xl mx-auto md:max-w-4xl border p-4">
+        <p class="text-2xl" v-if="condition.id === undefined">お譲り条件新規追加</p>
+        <p class="text-2xl" v-else>お譲り条件詳細</p>
+        <div class="mt-2">
+          <div class="grid grid-cols-1 gap-6">
+            <InputTextField v-model="condition.id" label="ID" type="text" readonly />
+            <InputTextField v-model="condition.conditionGroupId" label="お譲り条件グループID" type="text" />
+            <InputTextField v-model="condition.name" label="お譲り条件名" type="text" />
+            <InputTextField v-model="condition.orderNo" label="ソート順位" type="text" />
+            <div class="flex justify-between" v-if="condition.id === undefined">
+              <div></div>
+              <Button label="新規登録" color="green" :onClick="handlePostCondition" />
+            </div>
+            <div class="flex justify-between" v-else>
+              <Button label="削除" color="red" :onClick="handleDeleteCondition" />
+              <Button label="修正" color="green" :onClick="handlePutCondition" />
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script setup lang="ts">
+  import type { ConditionType } from '~/types'
+  import { useConditionsStore } from '~/stores/condition';
+  import InputTextField from '~/components/ui-part/form/InputTextField'
+  import Button from '~/components/ui-part/form/Button'
+
+  const { getConditions, postCondition, putCondition, deleteCondition } = useConditionsStore()
+
+  const { condition } = defineProps<{condition: ConditionType}>();
+
+  async function handlePostCondition() {
+    if(!confirm('新規追加しますか？')) return
+
+    await postCondition(condition)
+
+    navigateTo({
+      path: '/condition_groups/' + condition.conditionGroupId
+    });
+  }
+
+  async function handlePutCondition() {
+    if(!confirm('修正しますか？')) return
+    
+    await putCondition(condition)
+
+    navigateTo({
+      path: '/condition_groups/' + condition.conditionGroupId
+    });
+  }
+
+  async function handleDeleteCondition() {
+    if(!confirm('削除しますか？')) return
+
+    await deleteCondition(condition)
+
+    navigateTo({
+      path: '/condition_groups/' + condition.conditionGroupId
+    });
+  }
+</script>
---
+++ server/api/conditions/[id].put.ts
@@ -0,0 +1,19 @@
+import { camelToSnakeCase, snakeToCamelCase } from '~/utils'
+
+export default defineEventHandler(async (event) => {
+  const config = useRuntimeConfig()
+
+  const id: string | undefined = event.context.params?.id;
+  const body  = await readBody(event);
+  const condition = {
+      conditionGroupId: body.condition.conditionGroupId,
+      name: body.condition.name,
+      orderNo: body.condition.orderNo,
+  }
+  const response = await $fetch(config.apiBase + '/admin/conditions/' + id, {
+    method: 'PUT',
+    body: JSON.stringify(camelToSnakeCase({ condition }))
+  })
+
+  return snakeToCamelCase(response);
+});
